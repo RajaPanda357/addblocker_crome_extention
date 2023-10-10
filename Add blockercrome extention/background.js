592d7985f583119d9939893c9d78e930ed856cef
RajaPanda357 <147485483+RajaPanda357@users.noreply.github.com>
2023-10-10 13:51:06 +0530

    Add files via upload

---
+++ Add blockercrome extention/background.js
@@ -0,0 +1,21 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+    "*://*.youtube.com/*",
+    "*://*.primevideo.com/*",
+	"*://*.web.telegram.org/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
